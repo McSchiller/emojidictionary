c73a9f9514a65678c7d5500c1a8c555c43c9f3c4
Andreas Schiller <andreasschiller@Andreass-MBP.fritz.box>
2017-02-08 23:07:28 +0100

    neues tableview

--- DictionaryEmoji/ViewController.swift
+++ DictionaryEmoji/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var dacooltableview: UITableView!
 
+    var emojis = ["😁","😍","😬","🤓","😎","😂","😡","😘","😭","😊","😋","😅"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        dacooltableview.dataSource = self
+        dacooltableview.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
